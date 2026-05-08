214268c9e8141bd51ae0460160b4ae2f060ecb61
twn39 <twn39@163.com>
2026-05-08 14:46:40 +0800

    chore: restrict GRDB dependency to 7.x.x (upToNextMajor from 7.0.0)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/groue/GRDB.swift.git",
-            from: "7.10.0"
+            .upToNextMajor(from: "7.0.0")
         ),
     ],
     targets: [
